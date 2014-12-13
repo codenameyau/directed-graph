8f0d6fbb2edb4b219651f7eae0f860397c5e2971
Jorge Yau <jyau01@citymail.cuny.edu>
2014-12-12 21:17:45 -0500

    Adding mocha tests

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 graph.js
 ========
 
-node package for directed and undirected graphs
+node package for weighted directed and undirected graphs
+
+
+###Run tests:
+Make sure to have mocha installed: `npm install -g mocha`
+
+In project root directory, run `mocha test`
---
+++ test/test-vertex.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var assert = require('assert');
+var vertex = require('../src/vertex');
+
+
+/**********************
+ * Test Suite: Vertex *
+ **********************/
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
